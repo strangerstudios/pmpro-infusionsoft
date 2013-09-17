5244f03fa684c6e87a6cf1829b2e8fffe8fe5f51
Jason Coleman <jason@strangerstudios.com>
2013-09-17 00:12:23 +0000

    dummy data in conn.cfg.php

--- includes/conn.cfg.php
+++ includes/conn.cfg.php
@@ -1,5 +1,5 @@
 <?php
 
-$connInfo = array('connectionName:izm88326:i:7bc782d8afd75745a9e32cd9ca475e47:This is the connection for izm88326.infusionsoft.com');
+$connInfo = array('connectionName:{infusiongsoft id}:{infusionsoft api key}:This is the connection for {infusionsoft url}');
 
 ?>
